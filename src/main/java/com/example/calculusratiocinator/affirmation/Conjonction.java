d3b8e3e3db981174d42f314f79f535c5154cfd66
DinahFitiavana <hei.dinah.3@gmail.com>
2024-06-07 16:08:30 +0300

    fix: transform the class COnjonction to an abtract class

--- src/main/java/com/example/calculusratiocinator/affirmation/Conjonction.java
+++ src/main/java/com/example/calculusratiocinator/affirmation/Conjonction.java
@@ -5,36 +5,14 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Conjonction extends AffirmationParent{
+public abstract class Conjonction extends AffirmationParent{
     private AffirmationParent premièreAffirmationParent;
     private AffirmationParent deuxièmeAffirmationParent;
-    private String conjonction;
 
-    public Conjonction(AffirmationParent premièreAffirmationParent,
-                       AffirmationParent deuxièmeAffirmationParent, String conjonction) {
-        super(premièreAffirmationParent.getAffirmation()+" "+conjonction+" "+deuxièmeAffirmationParent.getAffirmation());
+    public Conjonction(String affirmation, AffirmationParent premièreAffirmationParent,
+                       AffirmationParent deuxièmeAffirmationParent) {
+        super(affirmation);
         this.premièreAffirmationParent = premièreAffirmationParent;
         this.deuxièmeAffirmationParent = deuxièmeAffirmationParent;
-        this.conjonction = conjonction;
-    }
-
-    @Override
-    public Boolean evaluerAffirmation() {
-        Boolean valeurPremièreAffirmation = premièreAffirmationParent.evaluerAffirmation();
-        Boolean valeurDeuxièmeAffirmation = deuxièmeAffirmationParent.evaluerAffirmation();
-
-        if (valeurPremièreAffirmation == null || valeurDeuxièmeAffirmation == null) {
-            return null;
-        }
-
-        if (conjonction == "et") {
-            return valeurPremièreAffirmation && valeurDeuxièmeAffirmation;
-        } else if (conjonction == "ou") {
-            return valeurPremièreAffirmation || valeurDeuxièmeAffirmation;
-        } else if (conjonction == "donc") {
-            return !valeurPremièreAffirmation || valeurDeuxièmeAffirmation;
-        } else {
-            return null;
-        }
     }
 }
